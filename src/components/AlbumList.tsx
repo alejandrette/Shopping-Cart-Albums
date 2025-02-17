25a6ac0440b596b2cf66d9a7382973bf2902f23e
Alejandrette <alejandrette5117@gmail.com>
2025-02-17 13:36:32 +0100

    fix filters and create pagination

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Loader } from "./Loader";
 
 interface Album {
@@ -13,31 +14,64 @@ interface AlbumListProps {
   loading: boolean;
 }
 
+const ITEMS_PER_PAGE = 9; // Número de álbumes por página
+
 export function AlbumList({ albums, loading }: AlbumListProps) {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calcular los índices para los álbumes a mostrar
+  const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
+  const currentAlbums = albums.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(albums.length / ITEMS_PER_PAGE);
 
   return (
     <>
       {loading 
         ? <Loader />
         : (
-          <div className="grid grid-cols-3 gap-6 justify-center mx-auto max-w-6xl mt-6 mb-6">
-            {albums.map(album => (
-              <div className="w-full max-w-sm border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700" key={album.id}>
+          <div className="flex flex-col items-center mx-auto max-w-6xl mt-6 mb-6">
+            <div className="grid grid-cols-3 gap-6 justify-center w-full">
+              {currentAlbums.map(album => (
+                <div className="w-full max-w-sm border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700" key={album.id}>
                   <a href={`https://www.last.fm/es/music/${album.author}/${album.title}`} target="_blank">
-                      <img className="p-8 rounded-t-lg" src={album.cover} alt="product image" />
+                    <img className="p-8 rounded-t-lg" src={album.cover} alt="product image" />
                   </a>
                   <div className="px-5 pb-5">
-                      <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{album.title}</h5>
-                      <div className="flex items-center mt-2.5 mb-5 text-gray-500 font-black">
-                          <span>{album.author}</span>
-                      </div>
-                      <div className="flex items-center justify-between">
-                          <span className="text-3xl font-bold text-gray-900 dark:text-white">${album.price}</span>
-                          <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                      </div>
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{album.title}</h5>
+                    <div className="flex items-center mt-2.5 mb-5 text-gray-500 font-black">
+                      <span>{album.author}</span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <span className="text-3xl font-bold text-gray-900 dark:text-white">${album.price}</span>
+                      <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                    </div>
                   </div>
-              </div>
-            ))}
+                </div>
+              ))}
+            </div>
+  
+            <div className="w-full flex justify-center mt-10">
+              <button
+                className="px-4 py-2 bg-gray-800 text-white rounded-md disabled:opacity-50"
+                onClick={() => setCurrentPage(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                Previous
+              </button>
+              <span className="px-4 py-2 bg-gray-600 text-white rounded-md mx-4">
+                Page {currentPage} of {totalPages}
+              </span>
+              <button
+                className="px-4 py-2 bg-gray-800 text-white rounded-md disabled:opacity-50"
+                onClick={() => setCurrentPage(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              >
+                Next
+              </button>
+            </div>
           </div>
         )
       }
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -10,27 +10,30 @@ interface FiltersProps {
 
 export function Filters({ filters, onAuthorChange, onMaxPriceChange, authorUnique }: FiltersProps) {
   return (
-    <div className="gap-6 justify-center max-w-sm mx-auto">
+    <div className="flex justify-between justify-center max-w-lg mx-auto gap-x-10 mt-3">
+      <div className="w-1/3 flex items-center">
       <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => onAuthorChange(e.target.value)}>
         <option value={'all'}>All Author</option>
         {authorUnique.map((author, index) => (
           <option key={index} value={author}>{author}</option>
         ))}
       </select>
+      </div>
 
-      <label className="text-lg font-semibold text-gray-700 dark:text-gray-300">
-        Max Price: <span className="text-blue-600 dark:text-blue-400">${filters.maxPrice}</span>
-      </label>
-      <input 
-        type="range"  
-        min="10" 
-        max="26"
-        step='2'
-        className="w-full max-w-sm h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-blue-500 
-               dark:bg-gray-700 dark:accent-blue-400"
-        value={filters.maxPrice}
-        onChange={(e) => onMaxPriceChange(Number(e.target.value))}
-      />
+      <div className="w-1/4 text-right">
+        <label className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+          Max Price: <span className="text-blue-600 dark:text-blue-400">${filters.maxPrice}</span>
+        </label>
+        <input 
+          type="range"  
+          min="10" 
+          max="26"
+          step='2'
+          className="w-full max-w-sm h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-blue-500 dark:bg-gray-700 dark:accent-blue-400"
+          value={filters.maxPrice}
+          onChange={(e) => onMaxPriceChange(Number(e.target.value))}
+        />
+      </div>
     </div>
   )
 }
